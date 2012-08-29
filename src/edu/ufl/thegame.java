9de8c18421041a176094775f9180fbe67da4eca1
Brian Picciano <bgp2396@bellsouth.net>
2012-08-29 01:29:27 -0400

    initial commit, with hello world!!!

---
+++ src/edu/ufl/thegame.java
@@ -0,0 +1,20 @@
+package edu.ufl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class thegame extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        TextView text = new TextView(this);
+        text.setText("Hello World!");
+        setContentView(text);
+    }
+}
