260b91ef0f9ef22a105aa6357ddceecbea95ba7e
Brian Picciano <bgp2396@bellsouth.net>
2012-09-11 01:12:39 -0400

    made all tabs into spaces

--- src/edu/ufl/GameThread.java
+++ src/edu/ufl/GameThread.java
@@ -14,47 +14,47 @@ public class GameThread extends Thread {
     long timeDiff;      // the time it took for the cycle to execute
     int sleepTime;      // ms to sleep (<0 if we're behind)
 
-	// Surface holder that can access the physical surface
-	private SurfaceHolder surfaceHolder;
-	// The actual view that handles inputs and draws to the surface
-	private GamePanel gamePanel;
-	// Albert is a rectangle?
-	public RectF albert;
-	// Albert moving 1 = right, -1 = left
-	public int moving;
+    // Surface holder that can access the physical surface
+    private SurfaceHolder surfaceHolder;
+    // The actual view that handles inputs and draws to the surface
+    private GamePanel gamePanel;
+    // Albert is a rectangle?
+    public RectF albert;
+    // Albert moving 1 = right, -1 = left
+    public int moving;
 
 
     /* Whether or not the thread is currently alive */
-	private boolean running;
-	public void setRunning(boolean running) {
-		this.running = running;
-	}
+    private boolean running;
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
 
-	public GameThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
-		super();
-		this.surfaceHolder = surfaceHolder;
-		this.gamePanel = gamePanel;
-		albert = new RectF(10,10,30,50);
-	}
+    public GameThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+        albert = new RectF(10,10,30,50);
+    }
 
-	@Override
-	public void run() {
-		long tickCount = 0L;
-		GameLog.d("GameThread", "Starting game loop");
-		while (running) {
+    @Override
+    public void run() {
+        long tickCount = 0L;
+        GameLog.d("GameThread", "Starting game loop");
+        while (running) {
             beginTime = System.currentTimeMillis();
 
             /* Begin actual game loop stuff */
 
-			tickCount++;
-			Canvas c = null;
-			try {
+            tickCount++;
+            Canvas c = null;
+            try {
                 c = surfaceHolder.lockCanvas();
                 synchronized (surfaceHolder) {
-                	// IF running - and not paused
-                	// Should use a "physics" method and give albert a speed and mulitply by elasped time
-                	albert.offset(5*moving, 0);
-                	// END IF
+                    // IF running - and not paused
+                    // Should use a "physics" method and give albert a speed and mulitply by elasped time
+                    albert.offset(5*moving, 0);
+                    // END IF
 
                     draw(c);
                 }
@@ -79,16 +79,14 @@ public class GameThread extends Thread {
                 } catch (InterruptedException e) { /* Oh well */ }
             }
 
-		}
-		GameLog.d("GameThread", "Game loop executed " + tickCount + " times");
-	}
-	
+        }
+        GameLog.d("GameThread", "Game loop executed " + tickCount + " times");
+    }
 
-	
-	private void draw(Canvas canvas) {
-		canvas.drawARGB(255, 255, 0, 0);
-		Paint color = new Paint();
-		color.setARGB(255, 0, 0, 255);
-		canvas.drawRect(albert, color);
-	}
+    private void draw(Canvas canvas) {
+        canvas.drawARGB(255, 255, 0, 0);
+        Paint color = new Paint();
+        color.setARGB(255, 0, 0, 255);
+        canvas.drawRect(albert, color);
+    }
 }
