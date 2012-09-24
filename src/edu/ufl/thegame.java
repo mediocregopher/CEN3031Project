6a8adcf579ff5e877cfa9877458f77c6a56e6c28
Sarah Hughes <sarahcantspel@gmail.com>
2012-09-23 23:29:09 -0400

    Splash Screen and Menu was created. Sounds were also partially added.

--- src/edu/ufl/thegame.java
+++ src/edu/ufl/thegame.java
@@ -1,11 +1,13 @@
 package edu.ufl;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class thegame extends Activity
 {
+	MediaPlayer song;
     /* Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -14,7 +16,15 @@ public class thegame extends Activity
         //setContentView(new GamePanel(this));
         setContentView(R.layout.main);
 
+        song = MediaPlayer.create(thegame.this, R.raw.go_gators_live);
+        song.start();
         
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+    
+    @Override
+    protected void onPause(){
+    	super.onPause();
+    	finish();
     }
 }
