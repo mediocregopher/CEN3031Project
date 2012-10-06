f8ea3920d2540c46f8d86934edeef3326fe22b9b
Brian Picciano <bgp2396@bellsouth.net>
2012-10-06 13:45:38 -0400

    fixed collision detection, it works with rectangles now. I was making things a lot harder then they needed to be before

--- src/edu/ufl/Util.java
+++ src/edu/ufl/Util.java
@@ -8,32 +8,23 @@ class Util {
      * Given two RectF's, detects whether or not they are colliding. If they aren't, returns
      * the NONE enum, otherwise returns the enum most closely matching which side of the second
      * RectF (b) the first RectF (a) is colliding with
-     *
-     * This function is a bit naive and doesn't quite work if your rectangles have very
-     * exageratted proportions (one being very long, for instance). In these cases detection
-     * of the collision still works, but direction guessing may be wrong. We won't be using
-     * many rectangles in the game anyway, but it's something to know.
      */
     public enum IntersectRet { NONE,TOP,BOTTOM,LEFT,RIGHT}
     public static IntersectRet intersect(RectF a, RectF b) {
-
-        if (a.left <= b.right  && a.right  >= b.left &&
-            a.top  <= b.bottom && a.bottom >= b.top     ) {
-
-            float xdiff =  (a.left + (a.right-a.left)/2f) - (b.left + (b.right-b.left)/2f);
-            float ydiff = (a.top  + (a.bottom-a.top)/2f) - (b.top  + (b.bottom-b.top)/2f);
-
-            if (Math.abs(xdiff) > Math.abs(ydiff)) {
-                if (xdiff < 0) { return IntersectRet.LEFT;  }
-                else           { return IntersectRet.RIGHT; }
+        RectF i = new RectF(a);
+        if (i.intersect(b)) {
+            if (i.width() > i.height()) {
+                if (a.top < b.top) return IntersectRet.TOP;
+                else return IntersectRet.BOTTOM;
             }
             else {
-                if (ydiff < 0) { return IntersectRet.TOP;    }
-                else           { return IntersectRet.BOTTOM; }
+                if (a.right > b.right) return IntersectRet.RIGHT;
+                else return IntersectRet.LEFT;
             }
-
         }
-        return IntersectRet.NONE;
+        else {
+            return IntersectRet.NONE;
+        }
     }
 
     /*
