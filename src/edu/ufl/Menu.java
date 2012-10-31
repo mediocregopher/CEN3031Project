13c81b928b3293b4c2459114de7935657ec730c1
Bryant <g8trbryantc@gmail.com>
2012-10-31 12:49:53 -0400

    Changed menu implementation

--- src/edu/ufl/Menu.java
+++ src/edu/ufl/Menu.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -16,35 +15,26 @@ public class Menu extends Activity
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
-		addListenerOnButton();
-		}
- 
-	public void addListenerOnButton() {
-		imageButton = (ImageButton) findViewById(R.id.playbutton);		
-		imageButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				try{
-					Class myClass = Class.forName("edu.ufl.thegame");
-					Intent intent = new Intent(Menu.this, myClass);
-					startActivity(intent);
-				}
-				catch(ClassNotFoundException e){
-			    	e.printStackTrace();
-			    	}
-//			  		Toast.makeText(Menu.this,"Play Button is Clicked", Toast.LENGTH_SHORT).show();
-				}
-			});
-		
-		imageButton = (ImageButton) findViewById(R.id.helpbutton);		
-		imageButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {				
-			  	Toast.makeText(Menu.this,"Use right and left side of touchscreen to navigate. Use bottom middle part of toughscreen to jump.", 
-			  			Toast.LENGTH_SHORT).show();
-				}
-			});
- 
-		}
+	}
+	
+	public void play(View v) {
+	    try{
+	        Class<?> myClass = Class.forName("edu.ufl.thegame");
+	        Intent intent = new Intent(Menu.this, myClass);
+	        startActivity(intent);
+	    }
+	    catch(ClassNotFoundException e){
+	        e.printStackTrace();
+	    }
+	    //          Toast.makeText(Menu.this,"Play Button is Clicked", Toast.LENGTH_SHORT).show();
+	}
 
+	
+	public void help(View v) {
+	    Toast.makeText(Menu.this,"Use right and left side of touchscreen to navigate. Use bottom middle part of toughscreen to jump.", 
+                Toast.LENGTH_SHORT).show();
+	}
+	
     @Override
     protected void onPause(){
     	super.onPause();
